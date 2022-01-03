d896ef8d2dcd393d028fd6d80a3e4da525eeb121
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2022-01-03 11:50:51 +0900

    Update util.py

--- util.py
+++ util.py
@@ -14,31 +14,9 @@ def add_hashtags(xs):
     return [f"#{x}" for x in xs]
 
 
-def get_hashtags():
-    return " OR ".join(add_hashtags(read_lines("hashtags.txt")))
+def join_or(xs):
+    return " OR ".join(xs)
 
 
-def get_evil_accounts():
-    return " ".join([f"-from:{x}" for x in read_lines("evil-accounts.txt")])
-
-
-def read_lines(filename):
-    with open(get_absolute_path(filename)) as f:
-        return [line.strip() for line in f.readlines()]
-
-    
-def read_file(filename):
-    with open(get_absolute_path(filename)) as f:
-        return f.read().strip()
-
-
-def write_file(filename, s):
-    with open(get_absolute_path(filename), "w") as f:
-        return f.write(str(s))
-
-
-def rewrite_file(filename, func):
-    with open(get_absolute_path(filename), "r+") as f:
-        s = func(f.read())
-        f.seek(0)
-        f.write(s)
+def format_unwanted_accounts(accounts):
+    return " ".join([f"-from:{account}" for account in accounts])
