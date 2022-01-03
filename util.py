bebb2b5b39e36d5ca01668523f325d572d925bb0
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2022-01-03 09:59:53 +0900

    Create util.py

---
+++ util.py
@@ -0,0 +1,10 @@
+def get_tens_minute():
+    return datetime.datetime.now().minute // 10
+
+
+def is_even(n):
+    return (n % 2) == 0
+
+
+def is_tens_minute_even():
+    return is_even(get_tens_minute())
