7155c018b6574e03a25c817c7cab7a7426dea62f
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2022-01-03 10:27:15 +0900

    Update util.py

--- util.py
+++ util.py
@@ -10,6 +10,16 @@ def is_tens_minute_even():
     return is_even(get_tens_minute())
 
 
+def read_file(filename):
+    with open(get_absolute_path(filename)) as f:
+        return f.read()
+
+
+def write_file(filename, s):
+    with open(get_absolute_path(filename), "w") as f:
+        return f.write(str(s))
+
+
 def rewrite_file(filename, func):
     with open(get_absolute_path(filename), "r+") as f:
         s = func(f.read())
