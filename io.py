695c2cc1a9eb4606d2329deb02cf4d509fbc0029
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2022-03-19 17:55:03 +0900

    Update io.py

--- io.py
+++ io.py
@@ -1,21 +1,16 @@
-# Usage: open_file("input.txt")
 def get_absolute_path(filename):
     return os.path.join(os.path.dirname(__file__), filename)
 
 
-# Usage: open_file("input.txt")
-def open_file(filename):
-    with open(filename) as f:
-        lines = [line.strip() for line in f.readlines()]
-        return " ".join(lines)
-
-
-# Usage: read_lines("input.txt")
 def read_lines(filename):
     with open(filename) as f:
         return [line.strip() for line in f.readlines()]
 
-    
+
+def read_lines_as_single_line(filename):
+    return " ".join(read_lines(filename))
+
+
 def read_file(filename):
     with open(get_absolute_path(filename)) as f:
         return f.read().strip()
